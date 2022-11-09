d45184833663aa4b9b8b9d7d338980a77ed0cff4
Alejandro Salas <asalas.wd@outlook.com>
2022-11-08 22:06:37 -0500

    Add Dimension object

--- src/com/java21days/CreateButtons.java
+++ src/com/java21days/CreateButtons.java
@@ -1,19 +1,20 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CreateButtons {
     public static void main(String[] args) {
+        Dimension d = new Dimension(1000, 1000)
         ArrayList<JButton> buttons = new ArrayList<JButton>();
         ArrayList<String> buttonText = new ArrayList(Arrays.asList("Save", "Update", "Cancel"));
         for (String button : buttonText) {
-            System.out.println(button);
+            JButton btn = new JButton(button);
+            buttons.add(btn);
         }
 
-
-
-//        ButtonsFrame frame = new ButtonsFrame();
+        ButtonsFrame frame = new ButtonsFrame();
     }
 }
