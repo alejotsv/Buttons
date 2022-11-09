1ea78988cd4786178cf5b582c0f706aa666773b4
Alejandro Salas <asalas.wd@outlook.com>
2022-11-08 22:04:06 -0500

    Add ArrayList with text for buttons

--- src/com/java21days/CreateButtons.java
+++ src/com/java21days/CreateButtons.java
@@ -1,4 +1,15 @@
 package com.java21days;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CreateButtons {
+    public static void main(String[] args) {
+        ArrayList<String> buttonText = new ArrayList(Arrays.asList("Save", "Update", "Cancel"));
+        for (String btn : buttonText) {
+            System.out.println(btn);
+        }
+
+//        ButtonsFrame frame = new ButtonsFrame();
+    }
 }
