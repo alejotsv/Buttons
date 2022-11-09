46e2e7d2f71b9941b9f1ee2bdd549ad41559b70e
Alejandro Salas <asalas.wd@outlook.com>
2022-11-08 22:05:11 -0500

    Create ArrayList to store JButtons

--- src/com/java21days/CreateButtons.java
+++ src/com/java21days/CreateButtons.java
@@ -1,14 +1,18 @@
 package com.java21days;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CreateButtons {
     public static void main(String[] args) {
+        ArrayList<JButton> buttons = new ArrayList<JButton>();
         ArrayList<String> buttonText = new ArrayList(Arrays.asList("Save", "Update", "Cancel"));
-        for (String btn : buttonText) {
-            System.out.println(btn);
+        for (String button : buttonText) {
+            System.out.println(button);
         }
+
+
 
 //        ButtonsFrame frame = new ButtonsFrame();
     }
